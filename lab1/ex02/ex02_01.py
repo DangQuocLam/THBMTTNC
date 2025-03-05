7fb7feb6de0a01241b31a3b5fb0d6da6e1b1be29
DangQuocLam <tranquoclama4@gmail.com>
2025-03-05 13:45:52 +0700

    Add files via upload

---
+++ lab1/ex02/ex02_01.py
@@ -0,0 +1,3 @@
+ten = input("Nhập tên: ")
+tuoi = input("Nhập tuổi: ")
+print("Tên là: ", ten, "Tuổi: ", tuoi)
---
+++ lab1/ex02/ex02_02.py
@@ -0,0 +1,3 @@
+ban_kinh = float(input("Nhập bán kính đường tròn: "))
+dien_tich = 3.14 * (ban_kinh ** 2)
+print("diện tích hình tròn là: ", dien_tich)
